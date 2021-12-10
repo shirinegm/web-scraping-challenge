6d856ce2973910048016aeab6d6a1cbef06e30b3
Shi Mas <shirinegm@gmail.com>
2021-12-09 18:26:04 -0600

    fixed returning only one news articles instead of 3

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, redirect
+from jinja2 import ChainableUndefined
 from flask_pymongo import PyMongo
 import scrape_mars
 import json
 
 app = Flask(__name__)
+app.jinja_env.undefined = ChainableUndefined
 
 # Use flask_pymongo to set up mongo connection
 mongo = PyMongo(app, uri="mongodb://localhost:27017/mission_db")
